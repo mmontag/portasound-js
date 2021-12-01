2ae70673d839a9c56e904f31ee737b0f20e8ed5c
Matt Montag <matt.montag@gmail.com>
2021-12-01 01:30:10 -0800

    Group TestBench sliders for better readability

--- src/TestBench.jsx
+++ src/TestBench.jsx
@@ -1,24 +1,32 @@
 import PortasoundSlider from './PortasoundSlider';
 import React from 'react';
 
+const SLIDERS_PER_GROUP = 8;
 export function TestBench(p) {
-  const sliders = [];
-  for (let i = 0; i < p.params.length; i++) {
-    sliders.push(
-      <>
-        <PortasoundSlider {...p.params[i]} value={p.values[i]} handleParamChange={p.handleParamChange} label={
-          <>
-            <b>{p.params[i].shortName}</b>
-            <span>{p.params[i].value.toString(2).padStart(4, '0')} <span style={{opacity: 0.5}}>{p.params[i].value}</span></span>
-          </>
-        }/>
-      </>
+  const groups = [];
+  for (let j = 0; j < p.params.length / SLIDERS_PER_GROUP; j++) {
+    const sliders = [];
+    for (let i = j * SLIDERS_PER_GROUP; i < p.params.length && i < (j+1) * SLIDERS_PER_GROUP; i++) {
+      sliders.push(
+        <>
+          <PortasoundSlider {...p.params[i]} value={p.values[i]} handleParamChange={p.handleParamChange} label={
+            <>
+              <b>{p.params[i].shortName}</b>
+              <span>{p.params[i].value.toString(2).padStart(4, '0')} <span
+                style={{ opacity: 0.5 }}>{p.params[i].value}</span></span>
+            </>
+          }/>
+        </>
+      );
+    }
+    groups.push(
+      <div className="param-subgroup">
+        {sliders}
+      </div>
     );
   }
   return <>
     <h2 className="param-group-label label">Parameter Test Bench</h2>
-    <div className="param-subgroup">
-      {sliders}
-    </div>
+    {groups}
   </>;
 }
