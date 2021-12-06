31b933bb764e6f1259b5968fc06cfcb2e2c68829
Matt Montag <matt.montag@gmail.com>
2021-12-06 01:04:11 -0800

    Improve PortasoundSlider.jsx

--- src/PortasoundSlider.jsx
+++ src/PortasoundSlider.jsx
@@ -24,15 +24,17 @@ class PortasoundSlider extends PureComponent {
   }
 
   render() {
-    const { value, range, shortName, idx, handleParamChange, label } = this.props;
+    const { value, range, name, shortName, idx, handleParamChange, label } = this.props;
     let ticks;
     if (range === 100) ticks = 11;
     else if (range === 128) ticks = 9;
     else if (range === 64) ticks = 9;
+    else if (range === 32) ticks = 17;
+    else if (range > 32) ticks = 8;
     else ticks = range;
 
     return (
-      <div key={'vert' + idx} className='vertical-slider-with-label'>
+      <div key={'vert' + idx} className='vertical-slider-with-label' title={name}>
         <div className='label'>{ label || shortName}</div>
         <div className='vertical-slider'>
           <div className='range-container'>
